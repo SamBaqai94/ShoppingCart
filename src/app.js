81e6b357b9a6a7d90a5133ec5e52db3d82a1afb4
SamBaqai94 <sameedbaqai94@gmail.com>
2020-09-21 03:22:22 -0400

    first push

---
+++ src/app.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+function Items(props) {
+    return (
+        <li className="list-group-item">
+            <p className="float-left">{props.value}</p>
+            <button className="btn btn-danger float-right" onClick={props.onClick}>Remove</button>
+        </li >
+    );
+}
+
+class ShoppingCart extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [
+                {
+                    'item': 'item1'
+                },
+                {
+                    'item': 'item2'
+                },
+
+            ]
+        }
+    }
+
+    renderList(item, step) {
+        return (
+            <Items key={step} value={item.item} onClick={() => this.removeItem(step)} />
+        )
+    }
+
+    removeItem(step) {
+        const copied = JSON.parse(JSON.stringify(this.state.items));
+        copied.splice(step, 1);
+        this.setState({
+            items: copied,
+        });
+    }
+
+    render() {
+        let status = "Number of Item in cart: " + (this.state.items.length > 0 ? this.state.items.length : "Cart is empty");
+        return (
+            <div>
+                <h2 className="text-center">Shopping Cart</h2>
+                <p>{status}</p>
+                <ul className="list-group">
+                    {this.state.items.map((item, step) => {
+                        return (this.renderList(item, step));
+                    })}
+
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default ShoppingCart;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import ShoppingCart from './app.js';
+
+ReactDOM.render(<ShoppingCart />, document.getElementById('root'));
